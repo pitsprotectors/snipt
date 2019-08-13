e62c04a16862e2b4f0d2114653c2fc6a5a38991e
sagarvadalia <sv344@cornell.edu>
2019-08-13 15:04:23 -0400

    clientside works for queries without redux

---
+++ client/components/project.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+const client = new ApolloClient()
+import {Link} from 'react-router-dom'
+
+export default class Project extends Component {
+  render() {
+    return (
+      <div>
+        <Link to={`/projects/${this.props.project.id}`}>
+          {' '}
+          {this.props.project.name}{' '}
+        </Link>
+      </div>
+    )
+  }
+}
